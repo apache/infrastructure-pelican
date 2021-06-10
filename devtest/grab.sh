1c2d77c73a43e2d5369f234d6a86621d2a0a8178
Greg Stein <gstein@gmail.com>
2021-06-10 01:35:28 -0500

    Begin adding stuff for dev/test.
    grab.sh will grab "all" the pelicanconf.py for review. This script
    likely has a short lifespan, as the goal is to remove pelicanconf

---
+++ devtest/grab.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# https://github.com/search?p=2&q=org%3Aapache+filename%3Apelicanconf.py&type=Code
+sites='template-site
+www-site
+ctakes-site
+openwebbeans-site
+river-site
+bval-site
+gora-site
+openjpa-site
+olingo-site
+infrastructure-website
+solr-site
+petri
+lucene-site
+treasurer-site
+fundraising-site
+xmlgraphics-website
+foundation-site
+openoffice-project
+diversity-site
+comdev-wwwsite'
+
+### this is not deployed as a real site.
+# infrastructure-p6/modules/pelican_asf/files
+
+#echo $sites
+
+# EXAMPLE:
+# https://raw.githubusercontent.com/apache/solr-site/main/pelicanconf.py
+
+for s in $sites; do
+    f="$s.pelicanconf.py"
+
+    branch=main
+    u="https://raw.githubusercontent.com/apache/$s/$branch/pelicanconf.py"
+    echo wget -O $f $u
+    wget -q -O $f $u
+
+    # The sites use "main" or "master". Try the other...
+    if [ -s $f ]; then true ; else
+        branch=master
+        u="https://raw.githubusercontent.com/apache/$s/$branch/pelicanconf.py"
+        echo RETRY: wget -O $f $u
+        wget -q -O $f $u
+    fi
+done
