f26f5cda8009debdae6ec41a68d21c3c47874338
Sebb <sebb@apache.org>
2023-06-16 16:40:19 +0100

    Some debug

--- tests/compare_meta.sh
+++ tests/compare_meta.sh
@@ -1,6 +1,6 @@
 echo Running GFM
-pelican content --settings pelican.gfm.py -d --o gfm
+pelican content --settings pelican.gfm.py -dD --o gfm
 echo Running default Markdown
-pelican content --settings pelican.def.py -d --o def
+pelican content --settings pelican.def.py -dD --o def
 rm def/sitemap.txt # not needed
 diff -r gfm def
