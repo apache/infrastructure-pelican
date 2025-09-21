501ffabe24e46a3de30692646221b521c044f822
Sebb <sebb@apache.org>
2025-09-21 11:52:51 +0100

    Fix missing test (inretiredgraduated_size)

--- tests/where/testasfdata.py
+++ tests/where/testasfdata.py
@@ -30,6 +30,7 @@ def test(name):
     assert metadata['graduated_count'] == metadata['graduated_size']
     assert metadata['retired_count'] == metadata['retired_size']
     assert metadata['notretired_size'] == metadata['graduated_size'] + metadata['current_size']
+    assert metadata['inretiredgraduated_size'] == metadata['graduated_size'] + metadata['retired_size']
     assert metadata['notinretiredgraduated_size'] == metadata['current_size']
     assert metadata['cttee_count'] == metadata['cttee_size']
     assert metadata['pmc_count'] == metadata['pmc_size']
